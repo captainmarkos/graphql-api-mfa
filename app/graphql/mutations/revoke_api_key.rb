edd8f35b7a6b364798842b1eadcd8fa507309e77
Mark Brettin <mark.brettin@rever.co>
2023-07-11 15:23:25 -0600

    revokeApiKey mutation tidy up

--- app/graphql/mutations/revoke_api_key.rb
+++ app/graphql/mutations/revoke_api_key.rb
@@ -1,7 +1,5 @@
-# Mutations::RevokeApiKey
 module Mutations
   class RevokeApiKey < Mutations::BaseMutation
-    #argument :params, Types::Input::NoteInputType, required: true
     argument :params, Types::Input::ApiUserInputType, required: true
 
     field :api_key, Types::ApiKeyType, null: true
@@ -10,9 +8,11 @@ module Mutations
       api_user_params = Hash params
 
       if (user = User.find_by(email: api_user_params[:email])).present?
-        # delete the newest api key
-        deleted_api_key = user.api_keys.newest.destroy
-        { api_key: deleted_api_key.as_json.merge(status: 'revoked') }
+        if (newest_key = user.api_keys.newest).present?
+          deleted_api_key = newest_key.destroy.as_json.merge(status: 'revoked')
+
+          { api_key: deleted_api_key }
+        end
       end
     rescue ActiveRecord::RecordInvalid => e
       GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
